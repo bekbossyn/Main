655ce10c4ad2249a2c81c88bc62a5a82bbfa3143
bekbossyn <bekbossyn.kassymkhan@gmail.com>
2022-03-18 14:16:21 +0900

    max depth

---
+++ src/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,10 @@
+public class MaximumDepthOfBinaryTree {
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        if (left ==0 || right == 0) return left + right + 1;
+        return Math.max(left, right) + 1;
+    }
+}
