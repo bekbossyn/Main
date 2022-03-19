5727714f467d49322b79754462263216eae11cc0
bekbossyn <bekbossyn.kassymkhan@gmail.com>
2022-03-19 12:09:22 +0900

    auto correct

--- src/SortedArrayToBST.java
+++ src/SortedArrayToBST.java
@@ -1,14 +1,15 @@
 public class SortedArrayToBST {
 
     public TreeNode sortedArrayToBST(int[] nums) {
-        return sortBTS(nums, 0, nums.length-1);
+        return sortBTS(nums, 0, nums.length - 1);
     }
-    public TreeNode sortBTS(int[] nums, int l, int r){
-        if (l >r) return null;
-        int m = (l + r +1)/ 2;
+
+    public TreeNode sortBTS(int[] nums, int l, int r) {
+        if (l > r) return null;
+        int m = (l + r + 1) / 2;
         TreeNode node = new TreeNode(nums[m]);
-        node.left = sortBTS(nums, l, m-1);
-        node.right = sortBTS(nums, m+1, r);
+        node.left = sortBTS(nums, l, m - 1);
+        node.right = sortBTS(nums, m + 1, r);
         return node;
     }
 }
