02bfed3423c6a89c1e605d686979db517dfe5b06
bekbossyn <bekbossyn.kassymkhan@gmail.com>
2022-03-19 23:04:27 +0900

    time usage

--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,19 @@ import java.util.*;
 import java.io.PrintWriter;
 
 public class Main {
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
         PrintWriter out = new PrintWriter(System.out);
 
-//        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime now = LocalDateTime.now();
 
 
-        
 
+        LocalDateTime now1 = LocalDateTime.now();
+        long secs = ChronoUnit.MILLIS.between(now, now1);
+        out.printf("%.3f", secs / 1000.0);
 
-//        LocalDateTime now1 = LocalDateTime.now();
-//        long secs = ChronoUnit.MILLIS.between(now, now1);
-//        out.printf("%.3f", secs/1000.0);
         in.close();
         out.close();
     }
