a79d7390a450cb3d0d51b1a32f4b25eee3b1a906
bekbossyn <bekbossyn.kassymkhan@gmail.com>
2022-03-21 18:36:40 +0900

    find final value

--- src/FindFinalValue.java
+++ src/FindFinalValue.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class FindFinalValue {
+public class FindFinalValue {
+
+    public int findFinalValue(int[] ns, int o) {
+        for (int i = 0; i < ns.length; i++) {
+            if (ns[i] == o) {
+                return findFinalValue(ns, o * 2);
+            }
+        }
+        return o;
+    }
+
 }
