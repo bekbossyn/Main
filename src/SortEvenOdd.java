6152496e965bf1fa7f559f044df5e3dabdeb373f
bekbossyn <bekbossyn.kassymkhan@gmail.com>
2022-03-21 18:35:36 +0900

    sort even odd

--- src/SortEvenOdd.java
+++ src/SortEvenOdd.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class SortEvenOdd {
+public class SortEvenOdd {
+
+    public int[] sortEvenOdd(int[] ns) {
+        int t;
+        for (int j = 0; j < ns.length; j++) {
+            for (int i = 0; i < ns.length - 1; i++) {
+                if (i + 2 < ns.length) {
+                    if (i % 2 == 0) {
+                        if (ns[i] > ns[i + 2]) {
+                            t = ns[i];
+                            ns[i] = ns[i + 2];
+                            ns[i + 2] = t;
+                        }
+                    } else {
+                        if (ns[i] < ns[i + 2]) {
+                            t = ns[i];
+                            ns[i] = ns[i + 2];
+                            ns[i + 2] = t;
+                        }
+                    }
+                }
+            }
+        }
+        return ns;
+    }
+
 }
