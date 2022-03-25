e9b6d8bc421a3b0e2c2edc3456c375d159397215
bekbossyn <bekbossyn.kassymkhan@gmail.com>
2022-03-25 16:14:45 +0900

    diagonal sum

--- src/DiagonalSum.java
+++ src/DiagonalSum.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class DiagonalSum {
+public class DiagonalSum {
+
+    public int diagonalSum(int[][] m) {
+        int sum = 0;
+        for (int i = 0; i < m.length; i++){
+            sum += m[i][i];
+            if (i != m.length - i - 1) sum+= m[i][m.length - i - 1];
+        }
+        return sum;
+    }
+
 }
