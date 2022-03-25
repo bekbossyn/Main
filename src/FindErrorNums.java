b16b7362a5fcb9ac9e53e813f8d835267827707f
bekbossyn <bekbossyn.kassymkhan@gmail.com>
2022-03-25 17:52:39 +0900

    Find error nums

--- src/FindErrorNums.java
+++ src/FindErrorNums.java
@@ -1,6 +1,20 @@
 public class FindErrorNums {
 
     public int[] findErrorNums(int[] ns) {
+        int duplicate = 0;
+        int missing = ns.length * (ns.length + 1) / 2;
+
+        for (int n : ns) {
+            if (ns[Math.abs(n) - 1] < 0) duplicate = Math.abs(n);
+            else ns[Math.abs(n) - 1] *= -1;
+        }
+        for (int i = 0; i < ns.length; i++)
+            if (ns[i] > 0) missing = i + 1;
+        return new int[]{duplicate, missing};
+    }
+
+    /*
+    public int[] findErrorNums(int[] ns) {
         int[] ms = new int[ns.length];
         for (int i = 0; i < ns.length; i++) ms[i] = i + 1;
         int missing = ns.length * (ns.length + 1) / 2;
@@ -15,5 +29,6 @@ public class FindErrorNums {
         }
         return new int[]{duplicate, missing};
     }
+    */
 
 }
