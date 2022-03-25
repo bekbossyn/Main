c58ba362a7fd536a147fe8d57b984a212b7e5ea0
bekbossyn <bekbossyn.kassymkhan@gmail.com>
2022-03-25 13:06:01 +0900

    missing number O(1) time

--- src/MissingNumber.java
+++ src/MissingNumber.java
@@ -2,7 +2,12 @@ import java.util.Arrays;
 import java.util.HashMap;
 
 public class MissingNumber {
-
+    public int missingNumber(int[] ns) {
+        int sum = ns.length * (ns.length + 1) / 2;
+        for (int n : ns) sum -= n;
+        return sum;
+    }
+    /* slow
     public int missingNumber(int[] ns) {
         HashMap <Integer,Boolean> h = new HashMap<>();
         for (int n:ns) h.put(n, true);
@@ -10,5 +15,6 @@ public class MissingNumber {
             if (!h.containsKey(i)) return i;
         return 0;
     }
+     */
 
 }
