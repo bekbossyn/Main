d27f9d09631dffedfd57fc266d51fd2965e284d7
bekbossyn <bekbossyn.kassymkhan@gmail.com>
2022-03-27 16:26:40 +0900

    is Palindrome faster

--- src/IsPalindrome.java
+++ src/IsPalindrome.java
@@ -5,6 +5,28 @@ import java.util.Queue;
 public class IsPalindrome {
 
     public boolean isPalindrome(ListNode head) {
+        if (head == null) return true;
+
+        ListNode slow = head, fast = head, rev = null;
+        while (fast != null && fast.next != null) {
+            ListNode t = rev;
+            rev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+            rev.next = t;
+        }
+
+        if (fast!= null) slow = slow.next;
+        while (rev != null){
+            if (rev.val != slow.val) return false;
+            rev=rev.next;
+            slow = slow.next;
+        }
+        return true;
+    }
+
+    /*
+    public boolean isPalindrome(ListNode head) {
         Queue<Integer> qs = new LinkedList<>();
         int cnt = 0;
         while (head != null) {
@@ -19,5 +41,6 @@ public class IsPalindrome {
             if (a[i] != a[cnt - i - 1]) return false;
         return true;
     }
+     */
 
 }
