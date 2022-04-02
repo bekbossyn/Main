3198e07fe1c933254a8683f288cdd8b5a8eecd33
bekbossyn <bekbossyn.kassymkhan@gmail.com>
2022-04-03 01:12:04 +0900

    triangular sum

--- src/TriangularSum.java
+++ src/TriangularSum.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class TriangularSum {
+public class TriangularSum {
+
+    public int triangularSum(int[] ns) {
+        if (ns.length == 1) return ns[0];
+        int[] ns1 = new int[ns.length - 1];
+        for (int i = 0; i < ns1.length; i++) ns1[i] = (ns[i] + ns[i + 1]) % 10;
+        return triangularSum(ns1);
+    }
+
 }
