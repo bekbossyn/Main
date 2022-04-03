11bbe00d1002217ed6b26792f213408a27f32069
bekbossyn <bekbossyn.kassymkhan@gmail.com>
2022-04-03 22:29:32 +0900

    cells in range

--- src/CellsInRange.java
+++ src/CellsInRange.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class CellsInRange {
+import java.util.ArrayList;
+import java.util.List;
+
+public class CellsInRange {
+
+    public List<String> cellsInRange(String s) {
+        List<String> ss = new ArrayList<>();
+        for (char c = s.charAt(0); c <= s.charAt(3); c++) {
+            for (int i = s.charAt(1) - 48; i <= s.charAt(4) - 48; i++) {
+                ss.add(c + Integer.toString(i));
+            }
+        }
+        return ss;
+    }
+
 }
