e6a7ae90040edf3e0b6d896a9e4e3d9b3854e152
bekbossyn <bekbossyn.kassymkhan@gmail.com>
2022-04-05 19:37:26 +0900

    climb stairs

--- src/ClimbStairs.java
+++ src/ClimbStairs.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class ClimbStairs {
+public class ClimbStairs {
+
+    public int climbStairs(int n) {
+        int[] ns = new int[46];
+        ns[0] = 1;
+        ns[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            ns[i] = ns[i - 1] + ns[i - 2];
+        }
+        return ns[n];
+    }
+
 }
