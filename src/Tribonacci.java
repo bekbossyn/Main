689bc7c501223225dce3bb90536dcba0ee3061e6
bekbossyn <bekbossyn.kassymkhan@gmail.com>
2022-04-05 19:32:58 +0900

    Tribonacci

---
+++ src/Tribonacci.java
@@ -0,0 +1,13 @@
+public class Tribonacci {
+
+    public int tribonacci(int n) {
+        int[] ns = new int[38];
+        ns[1] = 1;
+        ns[2] = 1;
+        for (int i = 3; i <= n; i++) {
+            ns[i] = ns[i - 1] + ns[i - 2] + ns[i - 3];
+        }
+        return ns[n];
+    }
+    
+}
