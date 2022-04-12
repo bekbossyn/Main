27b76e2ef1a9cbf5778f1b4fc7e04821d9d8b25d
bekbossyn <bekbossyn.kassymkhan@gmail.com>
2022-04-12 20:02:56 +0900

    fixed point

---
+++ src/FixedPoint.java
@@ -0,0 +1,9 @@
+public class FixedPoint {
+
+    public int fixedPoint(int[] arr) {
+        for (int i = 0; i < arr.length; i++)
+            if (i == arr[i]) return i;
+        return -1;
+    }
+
+}
