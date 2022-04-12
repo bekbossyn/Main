d66398f7c08fe421bf6b42f37a7a8a32458a7659
bekbossyn <bekbossyn.kassymkhan@gmail.com>
2022-04-12 17:01:40 +0900

    perfect number Euclid - Euler Theorem:

--- src/CheckPerfectNumber.java
+++ src/CheckPerfectNumber.java
@@ -1,6 +1,19 @@
 public class CheckPerfectNumber {
 
+    //  Euclid - Euler Theorem
+    // ([2^(p - 1)] * [2^p - 1] is an even perfect number
+    // except prime number 11!!!.
     public boolean checkPerfectNumber(int n) {
+        int[] ps = new int[]{2, 3, 5, 7, 13, 17, 19, 31};
+        for (int p : ps) {
+            if ((1 << (p - 1)) * ((1 << p) - 1) == n) return true;
+        }
+        return false;
+    }
+}
+
+/*
+public boolean checkPerfectNumber(int n) {
         int sum = 0, ns = n;
         for (int i = 1; i <= (int) Math.sqrt(ns); i++) {
             if (ns % i == 0) {
@@ -10,6 +23,6 @@ public class CheckPerfectNumber {
         }
         return sum == n && n > 1;
     }
-}
+ */
 
 
