bd4a722326e640f0660b07154807b4a22041dad8
bekbossyn <bekbossyn.kassymkhan@gmail.com>
2022-05-08 15:44:38 +0900

    Check Almost Equivaluent

--- src/CheckAlmostEquivalent.java
+++ src/CheckAlmostEquivalent.java
@@ -4,21 +4,10 @@ public class CheckAlmostEquivalent {
 
     public boolean checkAlmostEquivalent(String w1, String w2) {
 
-        HashMap<Character, Integer> h1 = new HashMap<>();
-        HashMap<Character, Integer> h2 = new HashMap<>();
-
-        for (char ch : w1.toCharArray()) h1.put(ch, h1.getOrDefault(ch, 0) + 1);
-        for (char ch : w2.toCharArray()) h2.put(ch, h2.getOrDefault(ch, 0) + 1);
-        for (char ch : w1.toCharArray()) {
-            int r1 = h1.getOrDefault(ch, 0);
-            int r2 = h2.getOrDefault(ch, 0);
-            if (Math.abs(r1 - r2) > 3) return false;
-        }
-        for (char ch : w2.toCharArray()) {
-            int r1 = h1.getOrDefault(ch, 0);
-            int r2 = h2.getOrDefault(ch, 0);
-            if (Math.abs(r1 - r2) > 3) return false;
-        }
+        HashMap<Character, Integer> h = new HashMap<>();
+        for (char ch : w1.toCharArray()) h.put(ch, h.getOrDefault(ch, 0) + 1);
+        for (char ch : w2.toCharArray()) h.put(ch, h.getOrDefault(ch, 0) - 1);
+        for (int i : h.values()) if (Math.abs(i) > 3) return false;
         return true;
     }
 
