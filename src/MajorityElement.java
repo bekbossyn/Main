2383921a517ae17142e1058dd672797ed8323a74
bekbossyn <bekbossyn.kassymkhan@gmail.com>
2022-05-08 10:32:58 +0900

    Majority Element 2

--- src/MajorityElement.java
+++ src/MajorityElement.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class MajorityElement {
 
@@ -8,5 +10,19 @@ public class MajorityElement {
         for (int n : ns) if (h.get(n) > ns.length / 2) return n;
         return 0;
     }
+
+    public List<Integer> MajorityElement(int[] ns) {
+        List l = new ArrayList<>();
+        HashMap<Integer, Integer> h = new HashMap<>();
+        for (int n : ns) h.put(n, h.getOrDefault(n, 0) + 1);
+        for (int n : ns) {
+            if (h.containsKey(n)) {
+                if (h.get(n) > ns.length / 3) l.add(n);
+                h.remove(n);
+            }
+        }
+        return l;
+    }
+
 
 }
