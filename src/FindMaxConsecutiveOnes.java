b743dbc6d72aff89c39172f537bb0891bef50ef8
bekbossyn <bekbossyn.kassymkhan@gmail.com>
2022-05-09 17:54:46 +0900

    Find max consecutive ones 2

--- src/FindMaxConsecutiveOnes.java
+++ src/FindMaxConsecutiveOnes.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class FindMaxConsecutiveOnes {
 
     public int findMaxConsecutiveOnes(int[] ns) {
@@ -14,6 +16,35 @@ public class FindMaxConsecutiveOnes {
             }
             max = Math.max(cnt, max);
         }
+        return max;
+    }
+
+    public int findMaxConsecutiveOnesII(int[] ns) {
+        int l = ns.length;
+        int[] b = new int[l];
+
+        if (ns[0] == 1) b[0] = 1;
+
+        int max = b[0];
+
+        for (int i = 1; i < l; i++) {
+            if (ns[i] == 1) b[i] = b[i - 1] + 1;
+            max = Math.max(b[i], max);
+        }
+        
+        if (max <l)
+            max = max + 1;
+
+        for (int i = l - 1; i > 0; i--)
+            if (b[i] > 0 && b[i - 1] > 0) b[i - 1] = b[i];
+
+        System.out.println(Arrays.toString(b));
+        for (int i = 1; i < l - 1; i++)
+            if (b[i] == 0 && b[i - 1] != 0 && b[i + 1] != 0) {
+                max = Math.max(max, b[i - 1] + b[i + 1] + 1);
+            }
+//        if (b[l - 1] != 0) max = Math.max(max, b[l - 1] + 1);
+//        if (b[l - 1] == 0 && l > 1 && b[l - 2] != 0) max = Math.max(max, b[l - 2] + 1);
         return max;
     }
 
