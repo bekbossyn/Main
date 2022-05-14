6ff37ddff16bf487f53042d7df7a83dffa778de6
bekbossyn <bekbossyn.kassymkhan@gmail.com>
2022-05-14 22:38:44 +0900

    Search in rotated array

--- src/Search.java
+++ src/Search.java
@@ -11,4 +11,23 @@ public class Search {
         return -1;
     }
 
+    public int searchII(int[] ns, int t) {
+        int j = 0;
+        while (j < ns.length - 1 && ns[j] < ns[j + 1]) j++;
+        j++;
+        int[] nn = new int[ns.length];
+        int ind = 0;
+        for (int i = j; i < ns.length; i++) nn[ind++] = ns[i];
+        for (int i = 0; i < j; i++) nn[ind++] = ns[i];
+        int l = 0, r = ns.length;
+        while (l < r) {
+            int mid = (l + r) / 2;
+            if (nn[mid] < t) l = mid + 1;
+            else r = mid;
+        }
+        for (int i = 0; i < nn.length; i++) if (ns[i] == t) ind = i;
+        if (l < nn.length && nn[l] == t) return ind;
+        return -1;
+    }
+
 }
