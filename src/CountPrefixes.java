7407ea10d5b516a4398f45a9fea862498aa2c44a
bekbossyn <bekbossyn.kassymkhan@gmail.com>
2022-05-18 19:09:51 +0900

    Count prefixes

---
+++ src/CountPrefixes.java
@@ -0,0 +1,10 @@
+public class CountPrefixes {
+
+    public int countPrefixes(String[] words, String s) {
+        int cnt = 0;
+        for (String w : words)
+            if (s.startsWith(w)) cnt++;
+        return cnt;
+    }
+
+}
