1e5ca545b1e60ebf37c6e6497bedb562c8855df3
bekbossyn <bekbossyn.kassymkhan@gmail.com>
2022-05-20 20:13:31 +0900

    Two Out of Three, space(O(1))

--- src/TwoOutOfThree.java
+++ src/TwoOutOfThree.java
@@ -4,29 +4,13 @@ public class TwoOutOfThree {
 
     public List<Integer> twoOutOfThree(int[] ns1, int[] ns2, int[] ns3) {
         List<Integer> l = new ArrayList<>();
-        HashMap<Integer, Integer> h = new HashMap<>();
-        for (int n : ns1) {
-            h.put(n, 1);
-//            hh.add(n);
+        byte[][] cnt = new byte[3][101];
+        for (int n : ns1) cnt[0][n] = 1;
+        for (int n : ns2) cnt[1][n] = 1;
+        for (int n : ns3) cnt[2][n] = 1;
+        for (int i = 1; i < 101; i++) {
+            if (cnt[0][i] + cnt[1][i] + cnt[2][i] > 1) l.add(i);
         }
-        HashSet<Integer> hh = new HashSet<>();
-        for (int n : ns2) {
-            if (!hh.contains(n)) {
-                h.put(n, h.getOrDefault(n, 0) + 1);
-            }
-            hh.add(n);
-        }
-        HashSet<Integer> hhh = new HashSet<>();
-        for (int n : ns3) {
-            if (!hhh.contains(n)) {
-                h.put(n, h.getOrDefault(n, 0) + 1);
-            }
-            hhh.add(n);
-        }
-        for (Map.Entry<Integer, Integer> i : h.entrySet()) {
-            if (i.getValue() >= 2) l.add(i.getKey());
-        }
-
         return l;
     }
 
