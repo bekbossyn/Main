b88f5fb25b83c7a5295408069d924097b0f8f326
fariza kurmangaliyev <bekbossyn.kassymkhan@gmail.com>
2022-08-28 13:11:25 +0900

    remove stars

--- src/RemoveStars.java
+++ src/RemoveStars.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class RemoveStars {
+import java.util.Stack;
+
+public class RemoveStars {
+
+    public String removeStars(String s) {
+        Stack<Character> ss = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '*') ss.pop();
+            else {
+                ss.push(c);
+            }
+        }
+        StringBuilder sss = new StringBuilder();
+        while (!ss.empty()) {
+            sss.append(ss.pop());
+        }
+        return sss.reverse().toString();
+    }
+
 }
