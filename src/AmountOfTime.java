cc8e3c5fb08a95e55da3e61ce5bb70f9c0e3fc30
bekbossyn <bekbossyn.kassymkhan@gmail.com>
2022-08-29 22:17:35 +0900

    trash

--- src/AmountOfTime.java
+++ src/AmountOfTime.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class AmountOfTime {
+public class AmountOfTime {
+
+    public int amountOfTime(TreeNode root, int start) {
+        int d = depth(root);
+        int dd = search(root, start);
+        return d + dd;
+    }
+
+    public int depth(TreeNode h) {
+
+        int l = 0;
+        int r = 0;
+        if (h.left != null) l = depth(h.left);
+        if (h.right != null) r = depth(h.right);
+        return Math.max(l, r) + 1;
+    }
+
+    public int search(TreeNode h, int s) {
+        int l = 0;
+        int r = 0;
+        if (h.left != null) l = depth(h.left);
+        if (h.right != null) r = depth(h.right);
+        if (h.val == s)
+            return Math.max(l, r) + 1;
+        return 0;
+    }
+
 }
