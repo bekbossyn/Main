fe6f1e15260efa0bc93cde78a1382a579ff55572
fariza kurmangaliyev <bekbossyn.kassymkhan@gmail.com>
2022-09-12 17:17:42 +0900

    digit count

---
+++ src/DigitCount.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class DigitCount {
+
+    public boolean digitCount(String num) {
+        int[] digits = new int[10];
+        for (int i = 0; i < 10; i++) digits[i] = 0;
+        for (int i = 0; i < num.length(); i++) {
+            digits[num.charAt(i) - '0']++;
+        }
+        System.out.println(Arrays.toString(digits));
+        for (int i = 0; i < num.length(); i++) {
+            if (digits[i] != num.charAt(i) - '0') return false;
+        }
+        return true;
+    }
+
+}
