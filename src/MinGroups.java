dc9bc732bc3f7840cbdf795a7c91516196cfb37e
fariza kurmangaliyev <bekbossyn.kassymkhan@gmail.com>
2022-09-12 14:31:07 +0900

    MinGroups

--- src/MinGroups.java
+++ src/MinGroups.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class MinGroups {
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class MinGroups {
+
+    public int minGroups(int[][] ints) {
+        Arrays.sort(ints, (a, b) -> Double.compare(a[0], b[0]));
+        int len = ints.length;
+        Queue<Integer> q = new PriorityQueue<>();
+        for (int[] i : ints) {
+            if (!q.isEmpty() && q.peek() < i[0]) q.remove();
+            System.out.println(q);
+            q.add(i[1]);
+            System.out.println(q);
+        }
+        return q.size();
+    }
+
 }
