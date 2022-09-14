fb584c987d59d1f6d54d55c2eb21c7edaa9b9788
fariza kurmangaliyev <bekbossyn.kassymkhan@gmail.com>
2022-09-14 18:47:12 +0900

    Min Meeting rooms

--- src/MinMeetingRooms.java
+++ src/MinMeetingRooms.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class MinMeetingRooms {
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+public class MinMeetingRooms {
+
+    public int minMeetingRooms(int[][] ints) {
+        Arrays.sort(ints, (a, b) -> Double.compare(a[0], b[0]));
+        PriorityQueue<Integer> q = new PriorityQueue<>();
+        for (int[] i : ints) {
+            if (!q.isEmpty() && q.peek() < i[0]) q.remove();
+            q.add(i[1]);
+        }
+        return q.size();
+    }
+
 }
