7df0820ee2c34fcd292b52357bf82f519e913430
fariza kurmangaliyev <bekbossyn.kassymkhan@gmail.com>
2022-09-18 20:52:45 +0900

    fizes

--- src/SumPrefixScores.java
+++ src/SumPrefixScores.java
@@ -1,41 +1,37 @@
-import java.util.Arrays;
-import java.util.HashMap;
+class Trie {
+    Trie []ch = new Trie[26];
+    int cnt = 0;
+}
 
 public class SumPrefixScores {
-
-    class Trie {
-        Trie[] ch = new Trie[26];
-        int count = 0;
-    }
 
     public int[] sumPrefixScores(String[] ws) {
         Trie trie = new Trie();
         int len = ws.length;
-        int[] sum = new int[len];
-        int k = 0;
-        for (String w : ws) {
+        int[] sum = new int [len];
+
+        for (String w : ws ) {
             Trie t = trie;
-            for (char c : w.toCharArray()) {
-                int v = c - 'a';
-                if (t.ch[v] == null) t.ch[v] = new Trie();
-                t.ch[v].count++;
-                t = t.ch[v];
+            for (char v : w.toCharArray()){
+                int c = v-'a';
+                if (t.ch[c]==null) t.ch[c] = new Trie();
+                t.ch[c].cnt++;
+                t = t.ch[c];
             }
         }
-
-        for (String w : ws) {
+        int index = 0;
+        for (String w:ws) {
             Trie t = trie;
             int total = 0;
-            for (char c : w.toCharArray()) {
-                int v = c - 'a';
-                total += t.ch[v].count;
-                t = t.ch[v];
+            for (char v : w.toCharArray()) {
+                int c = v-'a';
+                total+=t.ch[c].cnt;
+                t = t.ch[c];
             }
-            sum[k++] = total;
+            sum[index++] = total;
         }
 
         return sum;
     }
-
 
 }
