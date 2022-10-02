deb428fda01d7920651dd4ca42d39094043a5379
fariza kurmangaliyev <bekbossyn.kassymkhan@gmail.com>
2022-10-02 18:53:10 +0900

    trailing zeroes faster

--- src/TrailingZeroes.java
+++ src/TrailingZeroes.java
@@ -1,21 +1,7 @@
 public class TrailingZeroes {
 
     public int trailingZeroes(int n) {
-        int[] nn = new int[2];
-        for (int i = 2; i <= n; i++) {
-            int q = i;
-            while (q % 2 == 0 || q % 5 == 0) {
-                if (q % 2 == 0) {
-                    nn[0]++;
-                    q /= 2;
-                }
-                if (q % 5 == 0) {
-                    nn[1]++;
-                    q /= 5;
-                }
-            }
-        }
-        return Math.min(nn[0], nn[1]);
+        return n == 0 ? 0 : n / 5 + trailingZeroes(n / 5);
     }
 
 }
