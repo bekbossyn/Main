cb8bb5603dca3a62a9129e634e8dc003fbf4b171
fariza kurmangaliyev <bekbossyn.kassymkhan@gmail.com>
2022-11-27 16:50:26 +0900

    Number of Cuts

--- src/NumberOfCuts.java
+++ src/NumberOfCuts.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class NumberOfCuts {
+public class NumberOfCuts {
+
+    public int numberOfCuts(int n) {
+        if (n % 2 == 0) return n / 2;
+        if (n == 1) return 0;
+        return n;
+    }
+
 }
