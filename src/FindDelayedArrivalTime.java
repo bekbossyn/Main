e7b974804be3b9a8cf285444e4d95fadc94c5901
fariza kurmangaliyev <bekbossyn.kassymkhan@gmail.com>
2023-04-23 11:36:03 +0900

    Sum of Multiples

--- src/FindDelayedArrivalTime.java
+++ src/FindDelayedArrivalTime.java
@@ -1,4 +1,4 @@
-public class findDelayedArrivalTime {
+public class FindDelayedArrivalTime {
 
     public int findDelayedArrivalTime(int arrivalTime, int delayedTime) {
         return (arrivalTime + delayedTime) % 24;
