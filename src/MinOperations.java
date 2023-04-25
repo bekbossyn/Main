5f1c4145603a94084feee534a39fe1c8ac05b81a
fariza kurmangaliyev <bekbossyn.kassymkhan@gmail.com>
2023-04-25 16:10:13 +0900

    Min Operations

--- src/MinOperations.java
+++ src/MinOperations.java
@@ -22,4 +22,35 @@ public class MinOperations {
         return len;
     }
 
+    public int minOperations(int[] ns) {
+
+        int answer = 100;
+        int ones = 0;
+        int len = ns.length;
+        for (int n : ns) if (n == 1) ones++;
+        if (ones > 0) return len - ones;
+        for (int i = 0; i < len; i++) {
+            int num = ns[i];
+            for (int j = i + 1; j < len; j++) {
+                num = gcd(num, ns[j]);
+                if (num == 1) {
+                    answer = Math.min(answer, j - i);
+                    break;
+                }
+            }
+            if (num != 1) break;
+        }
+        if (answer == 100) return -1;
+        return answer + len - 1;
+    }
+
+    int gcd(int a, int b) {
+        while (b != 0) {
+            int c = a % b;
+            a = b;
+            b = c;
+        }
+        return a;
+    }
+
 }
