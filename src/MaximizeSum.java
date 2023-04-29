42cb1952c2a573d23b3f4577fb165a01716af2a7
bekbossyn <bekbossyn.kassymkhan@gmail.com>
2023-04-30 00:14:16 +0900

    Maximize Sum

--- src/MaximizeSum.java
+++ src/MaximizeSum.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class MaximizeSum {
+public class MaximizeSum {
+
+    public int maximizeSum(int[] ns, int k) {
+        int max = ns[0];
+        for (int n : ns) if (max < n) max = n;
+        int sum = 0;
+        while (k > 0) {
+            k--;
+            sum += max;
+            max++;
+        }
+        return sum;
+    }
+
 }
