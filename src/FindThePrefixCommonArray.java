094907c90d2e589dc1987b8b823ac9808b144bd5
bekbossyn <bekbossyn.kassymkhan@gmail.com>
2023-04-30 00:14:00 +0900

    Find the Prefix

--- src/FindThePrefixCommonArray.java
+++ src/FindThePrefixCommonArray.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class FindThePrefixCommonArray {
+public class FindThePrefixCommonArray {
+
+    public int[] findThePrefixCommonArray(int[] A, int[] B) {
+        int len = A.length;
+        int[] C = new int[len];
+        for (int i = 0; i < len; i++) {
+            int cnt = 0;
+            for (int j = 0; j <= i; j++)
+                for (int k = 0; k <= i; k++)
+                    if (A[j] == B[k]) {
+                        cnt++;
+                        break;
+                    }
+            C[i] = cnt;
+        }
+        return C;
+    }
+
 }
