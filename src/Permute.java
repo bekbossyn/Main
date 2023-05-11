c1f96f655613706003ef3c378e27bb26c88dc56b
bekbossyn <bekbossyn_kassymkhan@gmail.com>
2023-05-11 15:53:32 +0900

    asdf

--- src/Permute.java
+++ src/Permute.java
@@ -4,43 +4,32 @@ import java.util.List;
 
 public class Permute {
 
+    private List<List<Integer>> result;
+
     public List<List<Integer>> permute(int[] ns) {
-
-        List<List<Integer>> result = new ArrayList<>();
-
-        List<Integer> left = new ArrayList<>();
-        for (int n : ns) left.add(n);
-
-        helper(result, new ArrayList<>(), left, ns, 0);
-
+        result = new ArrayList<>();
+        backtrack(0, ns);
         return result;
     }
 
-    void helper(List<List<Integer>> result, List<Integer> listCopy, List<Integer> left, int[] ns, int pos) {
-        if (pos >= ns.length) {
-            result.add(new ArrayList<>(listCopy));
-            return;
+    void backtrack(int index, int[] ns) {
+        if (index == ns.length) {
+            List<Integer> list = new ArrayList<Integer>(ns.length);
+            for (int n : ns) list.add(n);
+            result.add(list);
+        } else {
+            for (int i = index; i < ns.length; i++) {
+                swap(i, index, ns);
+                backtrack(index + 1, ns);
+                swap(i, index, ns);
+            }
         }
+    }
 
-        for (int i = 0; i < left.size(); i++) {
-            int num = left.get(i);
-
-            //  version 1   Faster
-            List<Integer> list = new ArrayList<>(left);
-            list.remove(i);
-            //
-
-            //  version 2   Slower
-//            List<Integer> list = new ArrayList<>();
-//            for (int j = 0; j < left.size(); j++)
-//                if (j != i) list.add(left.get(j));
-            //
-
-            listCopy.add(num);
-            helper(result, listCopy, list, ns, pos + 1);
-            listCopy.remove(listCopy.size() - 1);
-        }
-
+    private void swap(int i, int j, int[] ns) {
+        int temp = ns[i];
+        ns[i] = ns[j];
+        ns[j] = temp;
     }
 
 }
