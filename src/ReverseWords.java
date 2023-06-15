e8b538bf7f61231a21ce23f7ca63dd0ee799f008
bekbossyn <bekbossyn.kassymkhan@gmail.com>
2023-06-16 04:19:24 +0900

    reverse words

--- src/ReverseWords.java
+++ src/ReverseWords.java
@@ -1,6 +1,29 @@
+import java.util.Arrays;
+import java.util.Collections;
+
 public class ReverseWords {
 
     public String reverseWords(String s) {
+        StringBuilder result = new StringBuilder();
+        int i = 0;
+        while (i < s.length() && s.charAt(i) == ' ') i++;
+        s = s.substring(i);
+        int j = s.length() - 1;
+        while (j > 0 && s.charAt(j) == ' ') j--;
+        s = s.substring(0, j + 1);
+        String[] ws = s.split("\\s+");
+        Collections.reverse(Arrays.asList(ws));
+        // System.out.println(s);
+        // System.out.println(Arrays.toString(ws));
+        for (String w : ws) {
+            result.append(w);
+            result.append(' ');
+        }
+        result = new StringBuilder(result.substring(0, result.length() - 1));
+        return result.toString();
+    }
+
+    public String reverseWords1(String s) {
         String[] ws = s.split("\\s+");
         StringBuilder sss = new StringBuilder();
         for (String w : ws) {
