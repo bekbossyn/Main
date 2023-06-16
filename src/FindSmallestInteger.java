e09a97bc817b21ce21a68894764da91053020d5c
bekbossyn <bekbossyn.kassymkhan@gmail.com>
2023-06-16 19:53:25 +0900

    asd

--- src/FindSmallestInteger.java
+++ src/FindSmallestInteger.java
@@ -4,7 +4,8 @@ import java.util.HashSet;
 public class FindSmallestInteger {
 
     public int findSmallestInteger(int[] A, int k) {
-        int stop = 0, count[] = new int[k];
+        int stop = 0;
+        int[] count = new int[k];
         for (int a : A)
             count[(a % k + k) % k]++;
         for (int i = 0; i < k; ++i)
