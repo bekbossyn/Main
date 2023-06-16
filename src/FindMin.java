c1b381356a8733e0752df28fb37a0702170de646
bekbossyn <bekbossyn.kassymkhan@gmail.com>
2023-06-16 10:11:53 +0900

    Find Min

--- src/FindMin.java
+++ src/FindMin.java
@@ -7,6 +7,21 @@ public class FindMin {
             int mid = l + (r - l) / 2;
             if (ns[mid] > ns[r])
                 l = mid + 1;
+            else if (ns[mid] == ns[r])
+                r = r - 1;
+            else
+                r = mid;
+        }
+        return ns[r];
+    }
+
+    public int findMin1(int[] ns) {
+        int len = ns.length;
+        int l = 0, r = len - 1;
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (ns[mid] > ns[r])
+                l = mid + 1;
             else
                 r = mid;
         }
