74e31080ae8ed02f84dfb3efd86d6d185a2d9f82
bekbossyn <bekbossyn.kassymkhan@gmail.com>
2023-06-16 19:15:45 +0900

    Search Matrix

--- src/SearchMatrix.java
+++ src/SearchMatrix.java
@@ -1,8 +1,33 @@
 import java.util.Arrays;
 
 public class SearchMatrix {
-    // binary method
+
     public boolean searchMatrix(int[][] ms, int t) {
+        int n = ms.length;
+        int m = ms[0].length;
+
+        int i = 0;
+        while (i < m) {
+            int l = 0, r = n - 1;
+            while (l < r) {
+                int mid = (l + r) / 2;
+                int num = ms[mid][i];
+                if (t > num) {
+                    l = mid + 1;
+                } else if (t < num) {
+                    r = mid - 1;
+                } else {
+                    return true;
+                }
+            }
+            if ((l < n && ms[l][i] == t) || (r >= 0 && ms[r][i] == t)) return true;
+            i++;
+        }
+        return false;
+    }
+
+    // binary method
+    public boolean searchMatrix1(int[][] ms, int t) {
         int n = ms.length;
         int m = ms[0].length;
 
